696eb533ef988964dbd6a48b8d0f9dc12d979052
lateNightDebug <luna.mccormick@edu.sait.ca>
2026-02-05 12:05:17 -0700

    adjusted week 4

--- app/week-4/new-item.tsx
+++ app/week-4/new-item.tsx
@@ -8,13 +8,19 @@ type item = {
 }
 
 export default function NewItem() {
+
     const[name, setName] = useState<string>("");
     const[quantity, setQuantity] = useState<string>("");
     const[category, setCategory] = useState<string>("produce")
     const[nameTouched,setTouched]= useState<boolean>(false)
+
     const handleSubmit = (e: React.FormEvent) =>{
         e.preventDefault(); 
-        console.log(typeof(quantity))
+        
+        if (!name || name.length<2){
+            document.getElementById("error").innerHTML = "name must be longer than 1 character"
+        }
+        else {
         console.log(name,quantity,category)
         let newItem = {name: name, quantity: quantity, category: category}
         alert(`Item ${newItem.name} quantity: ${newItem.quantity} Category: ${newItem.category} has been added to the shopping list`)
@@ -22,16 +28,31 @@ export default function NewItem() {
         setName("")
         setQuantity("")
         setCategory("")
-    }
+        setTouched(false)
+        document.getElementById("error").innerHTML = ""
+        document.getElementById("submit")?.setAttribute("submit",'disabled');
+        }
+    };
     const nameHandler = (e: React.ChangeEvent<HTMLInputElement>)=>{
         setName(e.target.value)
     }
     const quantityHandler= (e: React.ChangeEvent<HTMLInputElement>)=>{
         setQuantity(e.target.value)
     }
-    const categoryHandler= (e: React.ChangeEvent<HTMLInputElement>)=>{
+    const categoryHandler= (e: React.ChangeEvent<HTMLSelectElement>)=>{
         setCategory(e.target.value)
         
+    }
+    const handleTouch = () =>{
+        if (name=="") {
+            setTouched(true);
+            document.getElementById("error").innerHTML = "you must enter a name"
+        }
+        else{
+            setTouched(false)
+            document.getElementById("submit")?.removeAttribute('disabled');
+        }
+
     }
     
     return(
@@ -40,18 +61,24 @@ export default function NewItem() {
             <br></br>
             <div className="flex justify-center">
             <form className="" onSubmit={handleSubmit}>
+
             <div className="flex-1 mb-2">
             <label> Enter Item Name:</label>
-            <input required type="text" name="newName" value={name} onChange={nameHandler} className="border ml-2"/>
+            <input required type="text" name="newName" value={name} onChange={nameHandler} className={`border-2 ${nameTouched==false? 'border-white': 'border-red-600'}`}
+            onBlur={handleTouch}/>
+            <p 
+            className="text-red-500"
+            id="error"> </p>
             </div>
+            
             <div className="flex-1 mb-2">
             <label>Enter Item quantity:</label>
             <input required type="text" name="newQuantity"  min="1" max="99999" onChange={quantityHandler} className="border ml-2"/>
             </div>
             <div className="flex-1 mb-2">
             <label>Enter category</label>
-            <select name="category" className="border ml-5">
-                <option value="Produce" className="text-white bg-black hover:bg-grey-700 hover:bg-blue-950">produce</option>
+            <select name="category" className="border ml-5" onChange={categoryHandler}>
+                <option value="Produce" className="text-white bg-black hover:bg-blue-950">produce</option>
                 <option value="Dairy" className="text-white bg-black">Dairy</option>
                 <option value="Bakery" className="text-white bg-black">Bakery</option>
                 <option value="Meat" className="text-white bg-black">Meat</option>
@@ -65,9 +92,11 @@ export default function NewItem() {
             </select>
             </div>
             <br></br>
-            <div className="flex-1 mb-2 justify-items-center">
-            <button type="submit" className= "bg-blue-600 p-2 border rounded-2xl">add Item</button>
+
+            <div className=" mb-2 justify-content-center">
+            <button type="submit" id="submit" className= "bg-green-600 disabled:bg-gray-500 p-2 border rounded-2xl" disabled>add Item</button>
             </div>
+
             </form>
             </div>
             </div>
---
+++ app/week-5/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function week4(){
+    return(
+        <div>
+            
+            <div>
+                <Link className= "flex justify-center" href="/"><u>Return Home</u></Link>
+            </div>
+        </div>
+    )
+}
